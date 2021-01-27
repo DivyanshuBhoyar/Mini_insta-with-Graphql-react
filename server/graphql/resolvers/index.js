b5ccf709151a3752bfa25d9abb10c26e8c378ab2
Divyanshu Bhoyar <divyanshubhoyar@yahoo.com>
2021-01-27 12:23:48 +0530

    like/commentCount enabled from server side

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -5,7 +5,10 @@ const commentResolvers = require("./commentResolvers");
 //exports from the resolvers package
 module.exports = {
 	//to reduce client side computation , count is calculated here ::optional
-
+  Post: {
+    likeCount: (parent) => parent.likes.length,
+    commentCount: (parent) => parent.comments.length
+  },
   //rsolvers for query of all types
   Query: {
     ...postsResolvers.Query,
