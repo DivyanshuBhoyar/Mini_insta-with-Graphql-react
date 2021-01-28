130ba0f9b154738da964cf563d65cbb95b4c6af2
Divyanshu Bhoyar <divyanshubhoyar@yahoo.com>
2021-01-28 10:38:59 +0530

    authHeaders injected

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  createHttpLink,
+  ApolloProvider,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
+const httpLink = createHttpLink({
+  uri: "http://localhost:5000/",
+});
 // const link = createHttpLink({ });
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("jwtToken");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
