52d7e729fb3483c133297fa0dd11569ddb5fa448
Divyanshu Bhoyar <divyanshubhoyar@yahoo.com>
2022-07-10 22:04:44 +0530

    mongo

--- index.js
+++ index.js
@@ -5,24 +5,25 @@ const { ApolloServer, PubSub } = require("apollo-server");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
-
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => ({ req }),
-
 });
 
-mongoose
-  .connect(process.env.DB_ACCESS, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MongoDB Atlas Connected");
-    return server.listen({ port: process.env.PORT });
-  })
-  .then((res) => {
-    console.log(`Apollo Server is up & running at ${res.url}`);
-  });
+try {
+  mongoose
+    .connect(process.env.DB_ACCESS, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("MongoDB Atlas Connected");
+      return server.listen({ port: process.env.PORT });
+    })
+    .then((res) => {
+      console.log(`Apollo Server is up & running at ${res.url}`);
+    });
+} catch (e) {
+  console.error("DB connection err", e);
+}
